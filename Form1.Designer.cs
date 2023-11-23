8d5161076916ad8717bbaad75cc3e1d674c5736e
Kertalo <koksharov-kir@yandex.ru>
2023-11-23 10:28:00 +0300

    add camera

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,9 +38,10 @@
             rotation = new Button();
             scale = new Button();
             panel1 = new Panel();
-            rZ = new TextBox();
-            rY = new TextBox();
-            rX = new TextBox();
+            transformOk = new Button();
+            uZ = new TextBox();
+            uY = new TextBox();
+            uX = new TextBox();
             pZ = new TextBox();
             pY = new TextBox();
             transform = new Label();
@@ -65,7 +66,9 @@
             rotationSplit = new TextBox();
             label3 = new Label();
             createRotation = new Button();
-            transformOk = new Button();
+            rX = new TextBox();
+            rY = new TextBox();
+            rZ = new TextBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
             panel1.SuspendLayout();
             panel2.SuspendLayout();
@@ -166,39 +169,52 @@
             // 
             // panel1
             // 
-            panel1.Controls.Add(transformOk);
             panel1.Controls.Add(rZ);
             panel1.Controls.Add(rY);
             panel1.Controls.Add(rX);
+            panel1.Controls.Add(transformOk);
+            panel1.Controls.Add(uZ);
+            panel1.Controls.Add(uY);
+            panel1.Controls.Add(uX);
             panel1.Controls.Add(pZ);
             panel1.Controls.Add(pY);
             panel1.Controls.Add(transform);
             panel1.Controls.Add(pX);
             panel1.Location = new Point(761, 8);
             panel1.Name = "panel1";
-            panel1.Size = new Size(239, 128);
+            panel1.Size = new Size(239, 171);
             panel1.TabIndex = 30;
             // 
-            // rZ
+            // transformOk
             // 
-            rZ.Location = new Point(184, 68);
-            rZ.Name = "rZ";
-            rZ.Size = new Size(55, 27);
-            rZ.TabIndex = 37;
+            transformOk.Location = new Point(92, 139);
+            transformOk.Name = "transformOk";
+            transformOk.Size = new Size(63, 29);
+            transformOk.TabIndex = 41;
+            transformOk.Text = "Ok";
+            transformOk.UseVisualStyleBackColor = true;
+            transformOk.Click += TransformOkClick;
             // 
-            // rY
+            // uZ
             // 
-            rY.Location = new Point(108, 68);
-            rY.Name = "rY";
-            rY.Size = new Size(55, 27);
-            rY.TabIndex = 36;
+            uZ.Location = new Point(184, 68);
+            uZ.Name = "uZ";
+            uZ.Size = new Size(55, 27);
+            uZ.TabIndex = 37;
             // 
-            // rX
+            // uY
             // 
-            rX.Location = new Point(28, 68);
-            rX.Name = "rX";
-            rX.Size = new Size(55, 27);
-            rX.TabIndex = 35;
+            uY.Location = new Point(108, 68);
+            uY.Name = "uY";
+            uY.Size = new Size(55, 27);
+            uY.TabIndex = 36;
+            // 
+            // uX
+            // 
+            uX.Location = new Point(28, 68);
+            uX.Name = "uX";
+            uX.Size = new Size(55, 27);
+            uX.TabIndex = 35;
             // 
             // pZ
             // 
@@ -304,7 +320,7 @@
             panel2.Controls.Add(graphX1);
             panel2.Controls.Add(label1);
             panel2.Controls.Add(createGraph);
-            panel2.Location = new Point(761, 144);
+            panel2.Location = new Point(761, 182);
             panel2.Name = "panel2";
             panel2.Size = new Size(239, 128);
             panel2.TabIndex = 38;
@@ -445,15 +461,26 @@
             createRotation.UseVisualStyleBackColor = true;
             createRotation.Click += CreateRotationButton;
             // 
-            // transformOk
+            // rX
             // 
-            transformOk.Location = new Point(92, 99);
-            transformOk.Name = "transformOk";
-            transformOk.Size = new Size(63, 29);
-            transformOk.TabIndex = 41;
-            transformOk.Text = "Ok";
-            transformOk.UseVisualStyleBackColor = true;
-            transformOk.Click += TransformOkClick;
+            rX.Location = new Point(28, 101);
+            rX.Name = "rX";
+            rX.Size = new Size(55, 27);
+            rX.TabIndex = 42;
+            // 
+            // rY
+            // 
+            rY.Location = new Point(108, 101);
+            rY.Name = "rY";
+            rY.Size = new Size(55, 27);
+            rY.TabIndex = 43;
+            // 
+            // rZ
+            // 
+            rZ.Location = new Point(184, 101);
+            rZ.Name = "rZ";
+            rZ.Size = new Size(55, 27);
+            rZ.TabIndex = 44;
             // 
             // Form
             // 
@@ -507,9 +534,9 @@
         private TextBox pX;
         private Button axonometric;
         private Button perspective;
-        private TextBox rZ;
-        private TextBox rY;
-        private TextBox rX;
+        private TextBox uZ;
+        private TextBox uY;
+        private TextBox uX;
         private TextBox pZ;
         private TextBox pY;
         private ComboBox comboBox;
@@ -531,5 +558,8 @@
         private Label label3;
         private Button createRotation;
         private Button transformOk;
+        private TextBox rZ;
+        private TextBox rY;
+        private TextBox rX;
     }
 }
